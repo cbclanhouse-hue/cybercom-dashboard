4187296d968c45b8597bbc8ea0e6893c1a44dd2f
Seu Nome <seu@email.com>
2026-05-13 14:14:23 -0300

    Atualização do projeto

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000;
 
 const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '',
   database: process.env.DB_NAME || 'cybercom_dashboard',
